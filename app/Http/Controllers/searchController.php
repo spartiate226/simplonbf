58e986558673cc89482edec8b01130e416199790
spartiate226 <59071983+spartiate226@users.noreply.github.com>
2020-06-18 02:54:44 +0100

    corections pour la barr de recherche

--- app/Http/Controllers/searchController.php
+++ app/Http/Controllers/searchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\apprenant;
+use App\evenement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,11 +12,11 @@ class searchController extends Controller
     function index($type,Request $request){
         $result=null;
         if($type=='evenement' || $type =='actualite'){
-            $result=DB::table('evenements')->where('titre','LIKE',"$request->search")->get();
+            $result=evenement::query()->where('titre','LIKE',"%{$request->search}%")->get();
             return view('searchevenement',['liste'=>$result]);
         }
         if($type=='apprenant'){
-            $result=DB::table('evenements')->where('nom','LIKE',"$request->search")->orWhere('prenom','LIKE',"$request->search")->get();
+            $result=apprenant::query()->where('nom','LIKE',"%{$request->search}%")->orWhere('prenom','LIKE',"%{$request->search}%")->get();
             return view('searchapprenant',['liste'=>$result]);
         }
     }
